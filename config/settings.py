51a14e49e82b4e753c08b6cd78219a30a3f467b2
Juan Francisco  Perdomo <juanperdomo918918@gmail.com>
2026-06-12 11:10:19 -0300

    fin

--- config/settings.py
+++ config/settings.py
@@ -136,13 +136,15 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = (
-    'whitenoise.storage.CompressedManifestStaticFilesStorage'
-)
-
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
